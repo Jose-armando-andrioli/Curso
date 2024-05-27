7a4f77c01a8fa6d5b0d4b4b30ead075fc5a7c945
José Armando Andrioli <jarmando8644@gmail.com>
2024-05-27 13:14:09 -0300

    Correção texto

--- Aula_10/app.py
+++ Aula_10/app.py
@@ -1,4 +1,4 @@
-# Set definido por {} e não tem indece e nã permote trocar o valor  e a cada execução traz desordenado
+# Set definido por {} e não tem indece e nã permite trocar o valor  e a cada execução traz desordenado
 
 
 # frutas = {"Pera","Laranla","Banana"}
